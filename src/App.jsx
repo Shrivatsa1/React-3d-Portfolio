4ed45b8343929af7d2e7823d486deedb666257fb
Shrivatsa <shrivatsa1srv@gmail.com>
2025-08-23 22:28:57 +0530

    Added contact me section and made mobile responsive page

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import Header from "./components/Header";
+import Header1 from "./components/Header1";
 import HeroSection from "./components/HeroSection";
 import ProjectsSection from "./components/ProjectsSection";
 
 export default function App() {
   return (
     <>
-    <Header />
+    {/* <Header /> */}
+    <Header1 />
     <HeroSection />
     {/* <ProjectsSection /> */}
     </>
