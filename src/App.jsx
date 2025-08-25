36e5d482ff3697ef8ec5fac199d057dc5522eded
Shrivatsa <shrivatsa1srv@gmail.com>
2025-08-25 16:02:06 +0530

    created project and footer section

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,30 @@
+import {useEffect} from "react";
+import {gsap} from "gsap";
+import { ScrollTrigger } from "gsap/ScrollTrigger";
+
+
 import AboutSection from "./components/AboutSection";
 import CustomCursor from "./components/CustomCursor";
 import Header from "./components/Header";
 import Header1 from "./components/Header1";
 import HeroSection from "./components/HeroSection";
 import ProjectsSection from "./components/ProjectsSection";
+import ProjectsSection1 from "./components/ProjectSection1";
+import Footer from "./components/Footer";
 
 export default function App() {
+  useEffect(()=>{
+    //Register ScrollTrigger plugin
+    gsap.registerPlugin(ScrollTrigger)
+
+    // Refresh SScrollTrigger when the page is fully loaded
+    ScrollTrigger.refresh()
+
+    //Clean up ScrollTrigger on Component unmount
+    return()=>{
+      ScrollTrigger.getAll().forEach((trigger)=>trigger.kill())
+    }
+  },[])
   return (
     <>
     {/* <Header /> */}
@@ -14,6 +33,8 @@ export default function App() {
     <CustomCursor />
     <AboutSection /> 
     {/* <ProjectsSection /> */}
+    <ProjectsSection1 />
+    <Footer />
     </>
   )
 }
--- src/components/ProjectSection1.jsx
+++ src/components/ProjectSection1.jsx
@@ -7,7 +7,7 @@ import pro2 from "/images/pro2.png";
 import pro3 from "/images/pro3.png";
 import { h1 } from "framer-motion/client";
 
-const ProjectsSection = () => {
+const ProjectsSection1 = () => {
     const sectionRef=useRef(null);
     const titleRef=useRef(null);
     const titleLineRef=useRef(null);
@@ -105,79 +105,11 @@ const ProjectsSection = () => {
             }
         )
 
-        //Parallax effect for the entire section
-        gsap.fromTo(
-            sectionRef.current,
-            {
-                backgroundPosition:"50% 0%"
-            },
-            {
-                backgroundPosition:"50% 100%",
-                ease:"none",
-                scrollTrigger:{
-                    trigger:sectionRef.current,
-                    start:"top bottom",
-                    end:"bottom top",
-                    scrub:true,
-                }
-            }
-        )
-
-        //Horizontal scrolling
-        // Create the horizontal scrolling animation
-
-        const horizontalScroll=gsap.to("panel",{
-            xPercent:-100 * (projectImages.length -1),
-            ease:"none",
-            scrollTrigger:{
-                trigger:triggerRef.current,
-                start:"top top",
-                end:()=>`+=${horizontalRef.current.offsetWidth}`,
-                pin:true,
-                crub:1,
-                snap:{
-                    spanTo:1 /(projectImages.length-1),
-                    duration:{main:0.2,max:0.3},
-                    delay:0.1,
-                },
-                invalidateOnRefresh:true, 
-                
-            }
-        })
-
-        //Image Animation
-        //Animate each image panel
-        const panels=gsap.utils.toArray(".panel")
-        panels.forEach((panel,i)=>{
-            const image=panel.querySelector(".project-image")
-            const imageTitle = panel.querySelector(".project-title")
-
-            // create a timeloine for each panel
-            const tl=gsap.timeline({
-                scrollTrigger:{
-                    trigger:panel,
-                    containerAnimation:horizontalScroll,
-                    start:"left right",
-                    end:"right left",
-                    scrub:true,
-
-
-                }
-            })
-
-            //Image scale and opacity animation
-            tl.fromTo(image,{scale:0,rotate:-20},{scale:1,rotate:1,duration:0.5,})
-
-            //Title animation if it exists
-            if(imageTitle){
-                tl.fromTo(imageTitle,{y:30,},{y:-100,duration:0.3,},0.2)
-            }
-        })
-
+       
 
 
           
-    },[projectImages.length])
+    })
   return (
     <section
         ref={sectionRef}
@@ -200,32 +132,31 @@ const ProjectsSection = () => {
 
         {/* Horizontal scroll section  */}
         
-        <div ref={triggerRef} className="overflow-hidden opacity-0">
-            <div ref={horizontalRef} className="horizontal-section flex md:w-[400%] w-[420%] ">
+        <div ref={triggerRef} className="overflow-hidden ">
+            <div ref={horizontalRef} className="horizontal-section flex ">
                 
                 {
                     
-                    projectImages.map((project)=>(
-
+                    
                         
                 
-                        <div key={project.id}
-                        className="panel relative flex  items-center justify-center ">
+                        <div key={projectImages[0].id}
+                        className="relative flex  items-center justify-center "  >
                             
-                            <div className="relative w-full h-full flex flex-col items-center justify-center p-4 sm:p-8 md:p-12">
+                            <div className="relative w-full h-full flex flex-col items-center justify-center p-4 sm:p-8 md:p-12 ">
                                 <img className="project-image max-w-full max-h-full rounded-2xl object-cover"
-                                    src={project.imgSrc}
+                                    src={projectImages[0].imgSrc}
                                     alt="Project-img"
                                 
                                 />
                                 
                                 <h2 className="border-black project-title flex items-center gap-3 md:text-3xl text-sm md:font-bold text-black mt-6 z-50 text-nowrap hover:text-gray-400 transition-colors duration-300 cursor-pointer">
-                                    {project.title}<SlShareAlt />
+                                    {projectImages[0].title}<SlShareAlt />
                                 </h2>
 
                             </div>
                         </div>    
-                    ))
+                    
                 }
             </div>
         </div>
@@ -234,4 +165,4 @@ const ProjectsSection = () => {
   )
 }
 
-export default ProjectsSection
+export default ProjectsSection1
